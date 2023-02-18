0f8c4f22cb4adc524bb955c6371fae3ebe33d626
ahmadzikri11 <zikriahmad805@gmail.com>
2023-02-18 22:37:42 +0700

    crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //view data
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    //view create data
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'status' => $request->status,
+            'slug' => Str::slug($request->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Data baru berhasil dibuat'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Teradapat masalah dalam memasukan data, Silahkan coba lagi !!'
+                ]);
+        }
+    }
+
+//view edit
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content,
+            'status' => $request->status,
+            'slug' => Str::slug($request->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Post has been updated successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem has occured, please try again'
+                ]);
+        }
+    }
+
+//hapus data
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Post has been deleted successfully'
+                ]);
+        } else {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'error' => 'Some problem has occurred, please try again'
+                ]);
+        }
+    }
+    
+}
